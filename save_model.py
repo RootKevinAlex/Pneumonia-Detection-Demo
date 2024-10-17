24fbdaedbfee20e1735dc98a561b734e1b8c9916
Kevin Alexander Escobar <108683485+RootEscobar@users.noreply.github.com>
2024-10-17 00:29:24 -0600

    Co-authored-by: ChcltMuffin <ChcltMuffin@users.noreply.github.com>

--- save_model.py
+++ save_model.py
@@ -1,11 +1,11 @@
+## This script is used to download the model and save it to the model store.
 from __future__ import annotations
-
 import PIL.Image
 import PIL.ImageOps
 import transformers
 
 import bentoml
-
+##use bentoML to save the model
 def download_model() -> int:
     test_path = "./samples/NORMAL2-IM-1427-0001.jpeg"
     extractor = transformers.ViTImageProcessor.from_pretrained(
@@ -15,15 +15,15 @@ def download_model() -> int:
         "nickmuchi/vit-finetuned-chest-xray-pneumonia"
     )
 
-    # preprocess image
+##print("Model:", model)
     im = PIL.Image.open(test_path)
     im = PIL.ImageOps.exif_transpose(im).convert("RGB")
     outputs = model(**extractor(images=im, return_tensors="pt"))
 
-    top_k = len(model.config.id2label)  # there are two classes, normal and pneumonia
+    top_k = len(model.config.id2label)  
     probs = outputs.logits.softmax(-1)[0]
     scores, ids = probs.topk(top_k)
-
+##print("Prediction:", [{"score": score, "label": model.config.id2label[id_]} for score, id_ in zip(scores.tolist(), ids.tolist())])
     print(
         "Prediction:",
         [
@@ -44,7 +44,7 @@ def download_model() -> int:
                 custom_objects={"id2label": model.config.id2label},
             ),
         )
-
+##print("Extractor:", extractor)
     try:
         bextractor = bentoml.transformers.get("vit-extractor-pneumonia")
         print("Extractor already saved to model store:", bextractor)
@@ -53,8 +53,8 @@ def download_model() -> int:
             "Saved model:",
             bentoml.transformers.save_model("vit-extractor-pneumonia", extractor),
         )
-    
     return 0
 
 if __name__ == "__main__":
     raise SystemExit(download_model())    
+##download_model()
